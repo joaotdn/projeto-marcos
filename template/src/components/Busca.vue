9731ffe72710d0efaaf8a1607e51b5965aa67bce
Joao Teodoro <joao.dantas@conductor.com.br>
2017-08-11 20:50:54 -0300

    wiki busca

---
+++ template/src/components/Busca.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<v-card>
+			<v-card-title>
+				<v-text-field append-icon="search" label="Buscar" single-line hide-details v-model="search"></v-text-field>
+			</v-card-title>
+		</v-card>
+	
+		<v-expansion-panel>
+			<v-expansion-panel-content v-for="(item,i) in items" :key="i">
+				<div slot="header">{{ item.name }}</div>
+				<v-card>
+					<v-card-text class="grey lighten-3">{{ item.content }}</v-card-text>
+				</v-card>
+			</v-expansion-panel-content>
+		</v-expansion-panel>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			search: '',
+			pagination: {},
+			items: [
+				{
+					value: false,
+					name: 'Fruta',
+					content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+				},
+				{
+					value: false,
+					name: 'Pessoa',
+					content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+				},
+				{
+					value: false,
+					name: 'Música',
+					content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+				},
+			]
+		}
+	}
+}
+</script>
--- template/src/main.js
+++ template/src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
+import VueRouter from 'vue-router'
+
+// components
+import Busca from './components/Busca.vue'
+import Home from './components/Home.vue'
 
 Vue.use(Vuetify)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/buscar', component: Busca },
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
